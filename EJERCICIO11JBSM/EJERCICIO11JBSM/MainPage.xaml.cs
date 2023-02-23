c33c84d1a9f212c8f71c83ba97402fad8c6f8923
BladimirSierra <sierrabladimir218@gmail.com>
2023-02-22 20:29:51 -0600

    Agregar archivos de proyecto.

---
+++ EJERCICIO11JBSM/EJERCICIO11JBSM/MainPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace EJERCICIO11JBSM
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnSuma_Clicked(object sender, EventArgs e)
+        {
+            try { 
+            Double total = 0;
+            total = Double.Parse(txt_numero1.Text) + Double.Parse(txt_numero2.Text);
+                await Navigation.PushModalAsync(new resultados(total.ToString()));
+            }
+            catch
+            {
+                await DisplayAlert("Aviso", "Favor ingresar los 2 numeros solicitados", "OK");
+            }
+        }
+
+        private async void btnResta_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Double total = 0;
+                total = Double.Parse(txt_numero1.Text) - Double.Parse(txt_numero2.Text);
+                await Navigation.PushModalAsync(new resultados(total.ToString()));
+            }
+            catch
+            {
+                await DisplayAlert("Aviso", "Favor ingresar los 2 numeros solicitados", "OK");
+            }
+        }
+
+        private async void btnMultiplicacion_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Double total = 0;
+                total = Double.Parse(txt_numero1.Text) * Double.Parse(txt_numero2.Text);
+                await Navigation.PushModalAsync(new resultados(total.ToString()));
+            }
+            catch
+            {
+                await DisplayAlert("Aviso", "Favor ingresar los 2 numeros solicitados", "OK");
+            }
+        }
+
+        private async void btnDivision_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Double total = 0;
+                total = Double.Parse(txt_numero1.Text) / Double.Parse(txt_numero2.Text);
+                await Navigation.PushModalAsync(new resultados(total.ToString()));
+            }
+            catch
+            {
+                await DisplayAlert("Aviso", "Favor ingresar los 2 numeros solicitados", "OK");
+            }
+        }
+    }
+}
---
+++ EJERCICIO11JBSM/EJERCICIO11JBSM/resultados.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EJERCICIO11JBSM
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class resultados : ContentPage
+    {
+        public resultados()
+        {
+            InitializeComponent();
+        }
+        public resultados(String resultados)
+        {
+            InitializeComponent();
+            lblResultados.Text = "El resultado es: " + resultados;
+
+        }
+    }
+}
